ff89ea576a9d063b3ed22653102033509c3798a6
kouki928 <furukawavo@gmail.com>
2024-06-14 17:48:33 +0900

    google map 実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,46 @@
+"use client"
 import Image from "next/image";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
 export default function Home() {
+  const googleMapAPI: any = process.env.NEXT_PUBLIC_GOOGLE_MAP_API;
+
+  const containerStyle = {
+    width: "100%",
+    height: "86vh",
+  };
+  
+  const center = {
+    lat: 34.7293466708865,
+    lng: 135.49939605607292,
+  };
+  
+  const zoom = 13;  
+
+  const render = (status: Status) => {
+    return <h1>{status}</h1>;
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'YOUR API KEY HERE',
+    libraries: ['geometry', 'drawing'],
+  });
+  
+
   return (
     <>
+      <Wrapper apiKey={googleMapAPI} render={render}>
+        { isLoaded ? 
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={zoom}
+        ></GoogleMap> : 
+        <></>
+        }
+      </Wrapper>
     </>
   );
 }
